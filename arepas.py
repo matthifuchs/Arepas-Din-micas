737ee9a27d47d8c3e783ff4a8069e3b46a0c4d31
Matthias Fuchs Broszkowski <matthias.fuchs@correo.unimet.edu.ve>
2024-09-16 11:03:48 -0400

    Add files via upload

---
+++ arepas.py
@@ -0,0 +1,18 @@
+agua = 1
+print("el agua es", agua)
+harina = 1
+print("harina es", harina)
+sal = 1/16/3
+print("la sal es", sal)
+aceite = 1/16
+
+bol = agua+harina+sal
+print("el bol tiene", bol)
+
+budare = bol+aceite
+print("en el budare hay", budare)
+
+budare = budare * 0.9
+
+plato= budare
+print("el plato tiene", plato)
